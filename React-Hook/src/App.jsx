ca0c9a0e0703f6b949fe5b94bf1a83a7271e35a5
habibulla552272 <mdhabibulla146425@gmail.com>
2024-12-15 23:50:20 +0600

    first commit

---
+++ React-Hook/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+
+      <div>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint error fuga laboriosam est aspernatur, cupiditate illo sit eos voluptatem eligendi facilis accusantium facere. Vitae quasi dolores architecto tempore quisquam?</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non molestias saepe qui ut fuga, ex quaerat soluta molestiae corrupti nulla eos consequatur quam vel cumque ipsa rem minus fugit? Est.</p>
+      </div>
+      
+
+    </>
+  )
+}
+
+export default App
